45562656491889d4851abb83dc4083da138012a6
rheber <rheber@rediffmail.com>
2017-12-03 13:53:54 +1100

    Birthday, echo and help commands

--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -1,13 +1,68 @@
 "use strict"
 
 $(function () {
-  var jqc = $('#console').jqconsole('Welcome to the terminal.\n', '>>>', '...');
-  var restartPrompt = function () {
+  var welcomeText = 'Welcome to the terminal. Enter "help" for instructions.\n\n';
+  var noFocus = false;
+  var jqc = $('#console').jqconsole(welcomeText, '>>> ', '... ', noFocus);
+
+  jqc.RegisterShortcut('A', function() {
+    jqc.MoveToStart();
+  });
+  jqc.RegisterShortcut('E', function() {
+    jqc.MoveToEnd();
+  });
+
+  function out(msg) {
+    jqc.Write(msg, 'out');
+  }
+  function err(msg) {
+    jqc.Write(msg, 'err');
+  }
+
+  function birthday() {
+    out(happyBirthday.command() + '\n');
+  }
+
+  function echo(parsed) {
+    for(var i=1; i<parsed.length; i++) {
+      out(parsed[i] + ' ');
+    }
+    out('\n');
+  }
+
+  function help() {
+    out('birthday\tGenerate a birthday message.\n');
+    out('echo [ARGS]\tPrint arguments.\n');
+    out('help\t\tShow this message.\n');
+  }
+
+  function parse(input) {
+    return input.split(/[ \t]+/);
+  }
+
+  function exec(input) {
+    var parsed = parse(input);
+    if(parsed.length === 0) {
+      return;
+    }
+    var cmd = parsed[0];
+    if(cmd === 'birthday') {
+      birthday();
+    } else if(cmd === 'echo') {
+      echo(parsed)
+    } else if(cmd === 'help') {
+      help();
+    } else {
+      err('Syntax error\n');
+    }
+  }
+
+  var repl = function () {
     var historyEnabled = true;
     jqc.Prompt(historyEnabled, function (input) {
-      jqc.Write(input + '\n', 'jqconsole-output');
-      restartPrompt();
+      exec(input);
+      repl();
     });
   };
-  restartPrompt();
+  repl();
 });
