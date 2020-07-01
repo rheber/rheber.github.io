56edb62525c9e16039793e45cdb16841621abf5b
rheber <rheber@rediffmail.com>
2020-07-01 19:05:30 +1000

    personal commands

--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -5,13 +5,10 @@ $(function () {
   const noFocus = false;
   const jqc = $('#console').jqconsole(welcomeText, '\n>>> ', '... ', noFocus);
 
-  const helpText = `
-This is the personal website of Robert Heber, software developer by vocation and swing dancer by avocation.
-
-My website is a shell because I am cool.
-
-Below is a list of available commands.
-`
+  const helpText =
+    'This is the personal website of Robert Heber, software developer by vocation and swing dancer by avocation.\n' +
+    '\n' +
+    'My website is a shell because I am cool.\n'
 
   jqc.RegisterShortcut('A', function() {
     jqc.MoveToStart();
@@ -31,6 +28,14 @@ Below is a list of available commands.
     jqc.Write(msg, 'err');
   }
 
+  function _visit(url) {
+    if(url.includes('://')) {
+      window.open(url);
+      return;
+    }
+    window.open(`https://${url}`);
+  }
+
   function echo(parsed) {
     for(let i=1; i<parsed.length; i++) {
       out(parsed[i] + ' ');
@@ -40,6 +45,13 @@ Below is a list of available commands.
 
   function help() {
     out(helpText);
+    out('\n');
+    out('Commands to get more information about me:\n');
+    out('\n');
+    out('github\t\tVisit my github profile.\n');
+    out('tryhackme\tVisit my tryhackme profile.\n');
+    out('\n');
+    out('Other commands:\n');
     out('\n');
     out('echo [ARGS]\tPrint arguments.\n');
     out('help\t\tShow this message.\n');
@@ -52,17 +64,24 @@ Below is a list of available commands.
       return;
     }
     const url = parsed[1]
-    if(url.includes('://')) {
-      window.open(url);
-      return;
-    }
-    window.open(`https://${url}`);
+    _visit(url);
+  }
+
+  function github() {
+    _visit('github.com/rheber');
+  }
+
+  function tryhackme() {
+    _visit('tryhackme.com/p/rockstep');
   }
 
   const cmds = {
     'echo': echo,
     'help': help,
     'visit': visit,
+
+    'github': github,
+    'tryhackme': tryhackme,
   };
 
   function parse(input) {
