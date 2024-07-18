aff1d71857b33857ba414f8c134e24edd13868ed
rheber <rheber@rediffmail.com>
2024-07-18 19:32:53 +1000

    index

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -143,6 +143,13 @@ declare module 'astro:content' {
   collection: "blog";
   data: InferEntrySchema<"blog">
 } & { render(): Render[".mdx"] };
+"frustrations-icon.mdx": {
+	id: "frustrations-icon.mdx";
+  slug: "frustrations-icon";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".mdx"] };
 "starting-blog.mdx": {
 	id: "starting-blog.mdx";
   slug: "starting-blog";
