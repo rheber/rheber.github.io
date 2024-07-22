f4e9ec61c25c3e65a1e707a36335a9e7a6d0e09a
rheber <rheber@rediffmail.com>
2024-07-22 20:59:02 +1000

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,6 +2,8 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
+const image = "android-chrome-512x512.png";
+
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
@@ -12,5 +14,13 @@ export async function GET(context) {
 			...post.data,
 			link: `/blog/${post.slug}/`,
 		})),
+		customData: `
+			<language>en-au</language>
+			<image>
+				<url>${context.site + image}</url>
+				<title>${SITE_TITLE}</title>
+				<link>${context.site}</link>
+			</image
+		`,
 	});
 }
