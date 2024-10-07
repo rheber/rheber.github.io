52775ee897bd1ad430798502d566ecadeb808d45
rheber <rheber@rediffmail.com>
2024-10-07 18:34:40 +1100

    syllabus

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -143,6 +143,13 @@ declare module 'astro:content' {
   collection: "blog";
   data: InferEntrySchema<"blog">
 } & { render(): Render[".mdx"] };
+"syllabus.mdx": {
+	id: "syllabus.mdx";
+  slug: "syllabus";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".mdx"] };
 };
 
 	};
